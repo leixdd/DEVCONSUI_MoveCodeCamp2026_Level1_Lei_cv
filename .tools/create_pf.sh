423813921ee26c360c6e4cbda5eae8d135bec640
Lei <lecias.ariel@gmail.com>
2026-02-10 15:11:11 +0800

    Enhance create_pf.sh to include timestamp in JSON filename, validate transaction success, and output portfolio object ID; update .gitignore to include .env files

--- .tools/create_pf.sh
+++ .tools/create_pf.sh
@@ -54,7 +54,9 @@ fi
 #obtain the "published-at" value from the Published.toml file
 SUI_CONTRACT_PACKAGE_ID=$(grep "published-at" "$SUI_CONTRACT_PATH/Published.toml" | awk -F'=' '{gsub(/"/,""); print $2}' | tr -d ' ')
 
-DATE_TODAY=$(date +%Y%m%d)
+DATE_TODAY_W_TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
+SUI_CONTRACT_TX_PORTFOLIOS_PATH="$SUI_CONTRACT_PATH/tx_portfolios/$DATE_TODAY_W_TIMESTAMP.json"
 
 # call the create_portfolio functon using sui client call command
 sui client call \
@@ -62,10 +64,28 @@ sui client call \
 --module portfolio \
 --function create_portfolio \
 --args $WALLET_ADDRESS $NAME $COURSE $SCHOOL $ABOUT $LINKEDIN_URL $GITHUB_URL $SKILLS \
---gas-budget 1000000000 \
-> "$SUI_CONTRACT_PATH/tx_portfolios/$DATE_TODAY.json"
+--gas-budget 10000000 \
+> "$SUI_CONTRACT_TX_PORTFOLIOS_PATH"
 
+# check if file has "ProgrammableTransaction"
+if [ ! -f "$SUI_CONTRACT_TX_PORTFOLIOS_PATH" ]; then
+echo "Transaction data not found"
+exit 1
+fi
+
+IS_SUCCESS=$(grep "ProgrammableTransaction" "$SUI_CONTRACT_TX_PORTFOLIOS_PATH")
+if [ -z "$IS_SUCCESS" ]; then
+echo "Transaction failed"
+exit 1
+fi
 
 echo "Portfolio created successfully"
-echo "Transaction data saved to: $SUI_CONTRACT_PATH/tx_portfolios/$DATE_TODAY.json"
+echo "Transaction data saved to: $SUI_CONTRACT_TX_PORTFOLIOS_PATH"
 
+SUI_PACKAGE_MOD_FN="$SUI_CONTRACT_PACKAGE_ID::portfolio::Portfolio"
+PORTFOLIO_FRONTEND_PATH="$PWD/portfolio_frontend"
+
+PORTFOLIO_OBJECT_ID=$(grep -B 10 "$SUI_PACKAGE_MOD_FN" "$SUI_CONTRACT_TX_PORTFOLIOS_PATH" | grep "objectId" | awk -F'"' '{print $4}')
+
+echo "Portfolio object ID: $PORTFOLIO_OBJECT_ID"
+echo "VITE_PORTFOLIO_OBJECT_ID=$PORTFOLIO_OBJECT_ID" > "$PORTFOLIO_FRONTEND_PATH/.env"
